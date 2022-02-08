900dceffd9cb601d91bd43b889c9fd2f2583d57b
vech <jmvalenciae@unal.edu.co>
2022-02-07 20:17:10 -0500

    Just a commit to be sure with the config

--- config.h
+++ config.h
@@ -6,17 +6,17 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono:size=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Ubuntu Mono:size=12:antialias=true:autohint=true";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Ubuntu Mono:size=16:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Ubuntu Mono:size=16:antialias=true:autohint=true";
+static const char col_gray1[]       = "#1e222a";
+static const char col_gray2[]       = "#2e323a";
+static const char col_gray3[]       = "#545862";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#61afef";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan },
 };
 
 /* tagging */
@@ -63,12 +63,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
+static const char *arxivcmd[]  = { "arxiv_dmenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,             		XK_o, 	   spawn,          {.v = browsercmd } },
+	{ MODKEY,             		XK_u, 	   spawn,          {.v = arxivcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
