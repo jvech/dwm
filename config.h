791adc28ba23c2d0a96aedcf2fa8bd368c47f1b8
jvech <jmvalenciae@unal.edu.co>
2022-04-21 16:55:24 -0500

    Full gaps patch added

--- config.h
+++ config.h
@@ -2,17 +2,18 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono:size=16:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Ubuntu Mono:size=16:antialias=true:autohint=true";
-static const char col_gray1[]       = "#1e222a";
-static const char col_gray2[]       = "#2e323a";
-static const char col_gray3[]       = "#545862";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#222222";
-static const char col_custm[]        = "#ffaaaa";
+static const char col_custm[]       = "#ffaaaa";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -62,8 +63,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_custm, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *browsercmd[]  = { "surf", "localhost:8888", NULL };
-static const char *browser2cmd[]  = { "firefox", "localhost:8888", NULL };
+static const char *browsercmd[]  = { "surf", "127.0.0.1:8888", NULL };
+static const char *browser2cmd[]  = { "firefox", "127.0.0.1:8888", NULL };
 static const char *arxivcmd[]  = { "arxiv_dmenu", NULL };
 
 static Key keys[] = {
@@ -94,6 +95,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_plus,  setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_plus,  setgaps,        {.i = gappx } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
